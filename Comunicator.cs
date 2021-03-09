29af6016f4f96307858c7b2e86dd070ea15b0d14
slav4ocom <38194163+slav4ocom@users.noreply.github.com>
2021-03-09 22:36:43 +0300

    Add files via upload

---
+++ Comunicator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terminal
+{
+    public static class Comunicator
+    {
+        private static readonly string _comName = "COM5";
+        private static readonly int _baudRate = 115200;
+        private static readonly Parity _parity = Parity.None;
+        private static readonly int _dataBits = 8;
+        private static readonly StopBits _stopBits = StopBits.One;
+        
+
+        static SerialPort myPort;
+
+        public static void InitPort()
+        {
+            myPort = new SerialPort(_comName, _baudRate, _parity, _dataBits, _stopBits);
+            myPort.Open();
+        }
+
+        public static void Close()
+        {
+
+            myPort.Close();
+            myPort.Dispose();
+        }
+
+        public static void ShowPortInfo()
+        {
+            Console.WriteLine(myPort.PortName + "\r\n"
+                + myPort.BaudRate + "\r\n"
+                + myPort.Parity + "\r\n"
+                + myPort.DataBits + "\r\n"
+                + myPort.StopBits + "\r\n"
+                );
+        }
+
+        public static async Task Receive()
+        {
+            byte[] receiveBuffer = new byte[4096];
+            var numBytesReaden = await myPort.BaseStream.ReadAsync(receiveBuffer);
+            //Console.WriteLine(numBytesReaden);
+            Console.Write(Encoding.UTF8.GetString(receiveBuffer));
+        }
+
+        
+
+        public static async Task SendString(string inputString)
+        {
+            if (inputString == "+++" || inputString == "\\0")
+            {
+                byte[] sendBuffer = Encoding.UTF8.GetBytes(inputString);
+                await myPort.BaseStream.WriteAsync(sendBuffer);
+            }
+            else
+            {
+                byte[] sendBuffer = Encoding.UTF8.GetBytes(inputString + "\r\n");
+                await myPort.BaseStream.WriteAsync(sendBuffer);
+
+            }
+        }
+
+       
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO.Ports;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Terminal
+{
+    class Program
+    {
+        
+
+        
+       
+
+        static async Task ReceiveCycle()
+        {
+            while (true)
+            {
+                await Comunicator.Receive();
+            }
+        }
+
+      
+        static async Task TransmitCycle()
+        {
+            while (true)
+            {
+                var atCommnad = Console.ReadLine();
+                if (atCommnad.ToLower() == "exit")
+                {
+                    Console.WriteLine("Goodbye :) ...");
+                    break;
+                }
+                else if (atCommnad.ToLower() == "cls".ToLower())
+                {
+                    Console.Clear();
+                }
+                else
+                {
+                    await Comunicator.SendString(atCommnad);
+
+                }
+
+            }
+        }
+        static async Task Main(string[] args)
+        {
+
+
+
+            Comunicator.InitPort();
+            Comunicator.ShowPortInfo();
+
+            Task.Run(ReceiveCycle);
+            await Task.Run(TransmitCycle);
+
+            Comunicator.Close();
+
+        }
+    }
+}
